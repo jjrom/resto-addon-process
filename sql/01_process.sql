a17a2f743978ff2d385159304b3567f27b717623
jjrom <jerome.gasperi@gmail.com>
2023-12-04 09:20:30 +0100

    Open API Processes in progress

--- sql/01_process.sql
+++ sql/01_process.sql
@@ -1,42 +1,56 @@
 
 --
--- Entities
+-- Process
 --
 CREATE TABLE IF NOT EXISTS __DATABASE_COMMON_SCHEMA__.process (
+    -- Unique identifier provided during creation
+    "id"                    TEXT PRIMARY KEY,
+    -- Owner reference resto.user
+    userid                  BIGINT,
+    title                   TEXT
+    description             TEXT,
+    version                 TEXT,
+    keywords                TEXT[],
+    metadata                JSON,
+    created                 TIMESTAMP,
+    jobControlOptions       TEXT[],
+    outputTransmission      TEXT[],
+    additionalParameters    JSON,
+    links                   JSON,    
+    inputs                  JSON,
+    outputs                 JSON
+);
 
+--
+-- Job
+--
+CREATE TABLE IF NOT EXISTS __DATABASE_COMMON_SCHEMA__.job (
     -- Unique identifier provided during creation
     "id"                TEXT PRIMARY KEY,
-
-    -- Process status as HTTP Status
-    status              INT DEFAULT 202,
-
-    -- Process title
-    title                TEXT,
-
-    -- Process description
-    description         TEXT,
-
-    -- Reference resto.user
+    -- Owner reference resto.user
     userid              BIGINT,
-
-    -- Timestamp of process creation
+    -- Reference process id
+    process_id          TEXT,
+    type                TEXT default 'process',
+    status              INT DEFAULT 202,
+    message             TEXT,
     created             TIMESTAMP,
-
-    -- Timestamp of process ending
+    started             TIMESTAMP,
     finished            TIMESTAMP,
-
-    -- Process input - can contains any property
-    input               JSON,
-
-    -- Process output
-    output              JSON
+    updated             TIMESTAMP,
+    progress            INTEGER,
+    response            TEXT default 'raw',
+    subscriber          JSON,
+    links               JSON,    
+    inputs              JSON,
+    outputs             JSON
 
 );
 
 --
 -- Indexes
 --
-CREATE INDEX IF NOT EXISTS idx_status_process ON __DATABASE_COMMON_SCHEMA__.process (status);
+CREATE INDEX IF NOT EXISTS idx_status_job ON __DATABASE_COMMON_SCHEMA__.job (status);
 
 
 
--- src/Process.php
+++ src/Process.php
@@ -110,9 +110,6 @@ class Process extends RestoAddOn
                 throw new Exception(409, 'Process already exist ' . $body['id']);
             }
 
-            // Launch process
-            $this->triggerProcess($input);
-
         } catch (Exception $e) {
             return RestoLogUtil::httpError($e->getCode(), $e->getMessage());
         }
@@ -125,9 +122,49 @@ class Process extends RestoAddOn
 
     /**
      *  @OA\Get(
+     *      path="/processes",
+     *      summary="Retrieve the list of available processes",
+     *      description="The list of processes contains a summary of each process the OGC API - Processes offers, including the link to a more detailed description of the process",
+     *      tags={"process"},
+     *      @OA\Response(
+     *          response="200",
+     *          description="process",
+     *          @OA\JsonContent(
+     *              @OA\Property(
+     *                  property="processes",
+     *                  type="array",
+     *                  description="Array of processes",
+     *                  @OA\Items(ref="#/components/schemas/Process")
+     *              )
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response="404",
+     *          description="Not Found",
+     *          @OA\JsonContent(ref="#/components/schemas/NotFoundError")
+     *      )
+     *  )
+     *  
+     *  @param array $params
+     */
+    public function getProcesses($params)
+    {
+        
+        $results = $this->context->dbDriver->fetch($this->context->dbDriver->query('SELECT id, status, title, description, userid as owner, to_iso8601(created) as created, to_iso8601(finished) as finished, input, output FROM ' . $this->context->dbDriver->commonSchema . '.process');
+
+        if (isset($results) && count($results) === 1) {
+            return $this->formatProcess($results[0]);
+        }
+        
+        return RestoLogUtil::httpError(404);
+    }
+
+
+    /**
+     *  @OA\Get(
      *      path="/processes/{processId}",
-     *      summary="Get process",
-     *      description="Get process identified by *processId*",
+     *      summary="Retrieve the process description",
+     *      description="The process description contains information about inputs and outputs and a link to the execution-endpoint for the process.",
      *      tags={"process"},
      *      @OA\Parameter(
      *         name="processId",
@@ -135,14 +172,14 @@ class Process extends RestoAddOn
      *         required=true,
      *         description="process's identifier",
      *         @OA\Schema(
-     *             type="integer"
+     *             type="string"
      *         )
      *      ),
      *      @OA\Response(
      *          response="200",
      *          description="process",
      *          @OA\JsonContent(
-     *              ref="#/components/schemas/OutputProcess"
+     *              ref="#/components/schemas/Process"
      *          )
      *      ),
      *      @OA\Response(
@@ -172,6 +209,81 @@ class Process extends RestoAddOn
         
         return RestoLogUtil::httpError(404);
     }
+
+    /**
+     *  @OA\Get(
+     *      path="/jobs",
+     *      summary="Retrieve the list of jobs",
+     *      description="Lists available jobs.",
+     *      tags={"process"},
+     *      @OA\Response(
+     *          response="200",
+     *          description="process",
+     *          @OA\JsonContent(
+     *              @OA\Property(
+     *                  property="jobs",
+     *                  type="array",
+     *                  description="Array of jobs",
+     *                  @OA\Items(ref="#/components/schemas/Job")
+     *              )
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response="404",
+     *          description="Not Found",
+     *          @OA\JsonContent(ref="#/components/schemas/NotFoundError")
+     *      )
+     *  )
+     *  
+     *  @param array $params
+     */
+    public function getJobs($params)
+    {
+        
+    }
+
+    /**
+     *  @OA\Get(
+     *      path="/jobs/{jobId}",
+     *      summary="Get job",
+     *      description="Shows the status of a job.",
+     *      tags={"process"},
+     *      @OA\Parameter(
+     *         name="jobId",
+     *         in="path",
+     *         required=true,
+     *         description="job's identifier",
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response="200",
+     *          description="process",
+     *          @OA\JsonContent(
+     *              ref="#/components/schemas/Job"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response="400",
+     *          description="Invalid *jobId*",
+     *          @OA\JsonContent(ref="#/components/schemas/BadRequestError")
+     *      ),
+     *      @OA\Response(
+     *          response="404",
+     *          description="Not Found",
+     *          @OA\JsonContent(ref="#/components/schemas/NotFoundError")
+     *      )
+     *  )
+     *  
+     *  @param array $params
+     */
+    public function getJob($params)
+    {
+
+    }
+
+
 
     /**
      * Format raw process from database
